03ffc1b3f838d86983c68cb33707a0941cb7e3e7
Jesse Vincent <jesse@fsck.com>
2026-02-21 23:52:32 -0800

    feat: add learn module barrel exports

---
+++ src/learn/index.ts
@@ -0,0 +1,8 @@
+export {
+	LearnProcess,
+	type LearnMutation,
+	type LearnProcessOptions,
+	type ProcessResult,
+} from "./learn-process.ts";
+export { MetricsStore } from "./metrics-store.ts";
+export { shouldLearn } from "./should-learn.ts";
